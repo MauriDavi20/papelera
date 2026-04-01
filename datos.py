0dbe87ea3d641c8e18f2096ddd23de78619f6420
MauriDavi20 <mauricioposada08@gmail.com>
2026-04-01 10:58:56 -0500

    Add files via upload

--- datos.py
+++ datos.py
@@ -1,5 +1,6 @@
 n = 60
 opcion = None
+lista = []
 while opcion != 4:
  print("="*n)
  print("PAPELERA".center(n))
@@ -16,22 +17,45 @@ while opcion != 4:
       print("="*n)
       print("AÑADIR DATOS".center(n))
       print("="*n)
-      
-      nombre = input("Ingrese su nombre: ")
-      print(f"Su nombre es")
+      eleccion = "si"
+      while eleccion == "si":
+          nombre = input("Ingrese su nombre: ")
+          datos = {"Nombre":nombre}
+          lista.append(datos)
+          print(f"Su nombre es: {nombre}")
+          eleccion = input("Desea seguir registrando? (si/no): ").lower()
       input("ingrese ENTER para volver al menu: ")
+      
+      
  elif opcion == 2:
      print("="*n)
      print("ELIMINAR DATOS".center(n))
      print("="*n)
      nombre = input("Ingrese su nombre: ")
+     for datos in lista:
+          if datos['Nombre'] == nombre:
+               lista.remove(datos)
+               print(f"El nombre: {datos['Nombre']} fue eliminado")
+          else:
+               print("Ese nombre no se encuentra registrado")
      input("ingrese ENTER para volver al menu: ")
+     
+     
  elif opcion == 3:
       print("="*n)
       print("BUSCAR DATOS".center(n))
       print("="*n)
       nombre = input("Ingrese su nombre: ")
+      
+      for datos in lista:
+          if datos['Nombre'] == nombre:
+             print(f"El nombre: {datos['Nombre']} se encuentra registrado") 
+             break
+      else:
+          print("Este nombre no se encuentra")  
+          
       input("ingrese ENTER para volver al menu: ")
+
  else:
     print("solo opcion entre 1 y 4")
     
